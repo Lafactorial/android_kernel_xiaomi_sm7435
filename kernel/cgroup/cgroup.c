77ff97f2e66f6daea87add9b1de1e5e833c2a63d
John Galt <johngaltfirstrun@gmail.com>
2025-09-26 07:55:48 -0400

    treewide: Add kprofiles support

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -4961,8 +4961,11 @@ static int cgroup_attach_permissions(struct cgroup *src_cgrp,
 	return ret;
 }
 
+extern int kp_active_mode(void);
+
 static ssize_t cgroup_procs_write(struct kernfs_open_file *of,
 				  char *buf, size_t nbytes, loff_t off)
+
 {
 	struct cgroup_file_ctx *ctx = of->priv;
 	struct cgroup *src_cgrp, *dst_cgrp;
@@ -5003,7 +5006,7 @@ static ssize_t cgroup_procs_write(struct kernfs_open_file *of,
 	/* This covers boosting for app launches and app transitions */
 	if (!ret &&
 	    !memcmp(of->kn->parent->name, "top-app", sizeof("top-app")) &&
-	    task_is_zygote(task->parent)) {
+	    task_is_zygote(task->parent) && kp_active_mode() != 1) {
 		cpu_boost_max(500);
 	}
 
